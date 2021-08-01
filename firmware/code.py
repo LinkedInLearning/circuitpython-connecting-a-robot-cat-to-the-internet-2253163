ac23964a7381dfc6b966347afd69d16d25bc0415
Charlyn Gonda <chardane@gmail.com>
2021-08-01 00:04:34 -0700

    Moving servos

--- firmware/code.py
+++ firmware/code.py
@@ -2,12 +2,59 @@ import board
 import time
 from digitalio import DigitalInOut, Direction
 
-led = DigitalInOut(board.D11)
-led.direction = Direction.OUTPUT 
+import pulseio
+from adafruit_motor import servo
 
-while True:
-    print("blink!")
-    led.value = True
-    time.sleep(1)
-    led.value = False
-    time.sleep(1)
+pwm1 = pulseio.PWMOut(board.D8, frequency=50)
+pwm2 = pulseio.PWMOut(board.D9, frequency=50)
+
+servo1 = servo.Servo(pwm1)
+servo2 = servo.Servo(pwm2)
+servo_pause = 0.5
+
+def move_both():
+    servo1.angle = 90
+    servo2.angle = 0
+    time.sleep(servo_pause)
+    
+    servo1.angle = 0
+    servo2.angle = 90
+    time.sleep(servo_pause)
+    
+    servo1.angle = 90
+    servo2.angle = 0
+    time.sleep(servo_pause)
+    
+    servo1.angle = 0
+    servo2.angle = 90
+    time.sleep(servo_pause)
+    
+def move_left():
+    servo1.angle = 90
+    time.sleep(servo_pause)
+    
+    servo1.angle = 0
+    time.sleep(servo_pause)
+    
+    servo1.angle = 90
+    time.sleep(servo_pause)
+    
+    servo1.angle = 0
+    time.sleep(servo_pause)
+    
+def move_right():
+    servo2.angle = 0
+    time.sleep(servo_pause)
+    
+    servo2.angle = 90
+    time.sleep(servo_pause)
+    
+    servo2.angle = 0
+    time.sleep(servo_pause)
+    
+    servo2.angle = 90
+    time.sleep(servo_pause)
+    
+move_both()
+move_left()
+move_right()

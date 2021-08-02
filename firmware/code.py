98dcfed883f193e038c32983b963a8b0dd2c5cac
Charlyn Gonda <chardane@gmail.com>
2021-08-01 23:00:45 -0700

    Setup connection to an MQTT topic

--- firmware/code.py
+++ firmware/code.py
@@ -10,6 +10,9 @@ from adafruit_esp32spi import adafruit_esp32spi
 import busio
 from secrets import secrets
 
+from adafruit_esp32spi import adafruit_esp32spi_socket as socket
+from adafruit_minimqtt import adafruit_minimqtt as mqtt
+
 cs = DigitalInOut(board.ESP_CS)
 busy = DigitalInOut(board.ESP_BUSY)
 reset = DigitalInOut(board.ESP_RESET)
@@ -28,44 +31,58 @@ def move_both():
     servo1.angle = 90
     servo2.angle = 0
     time.sleep(servo_pause)
-    
+
     servo1.angle = 0
     servo2.angle = 90
     time.sleep(servo_pause)
-    
+
     servo1.angle = 90
     servo2.angle = 0
     time.sleep(servo_pause)
-    
+
     servo1.angle = 0
     servo2.angle = 90
     time.sleep(servo_pause)
-    
+
 def move_left():
     servo1.angle = 90
     time.sleep(servo_pause)
-    
+
     servo1.angle = 0
     time.sleep(servo_pause)
-    
+
     servo1.angle = 90
     time.sleep(servo_pause)
-    
+
     servo1.angle = 0
     time.sleep(servo_pause)
-    
+
 def move_right():
     servo2.angle = 0
     time.sleep(servo_pause)
-    
+
     servo2.angle = 90
     time.sleep(servo_pause)
-    
+
     servo2.angle = 0
     time.sleep(servo_pause)
-    
+
     servo2.angle = 90
     time.sleep(servo_pause)
 
 wifi.connect()
-move_both()
+
+mqtt.set_socket(socket, esp)
+mqtt_client = mqtt.MQTT(broker="io.adafruit.com",
+                        username=secrets["aio_username"],
+                        password=secrets["aio_key"])
+
+mqtt_client.on_connect = connected
+mqtt_client.on_disconnect = disconnected
+mqtt_client.on_message = message
+
+mqtt_client.connect()
+
+while True:
+    mqtt_client.loop()
+    time.sleep(1)

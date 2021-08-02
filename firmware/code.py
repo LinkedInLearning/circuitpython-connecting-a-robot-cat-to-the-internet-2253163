7a5f9e5c17f7ca6da235672fe877b03d2f9a5a4c
Charlyn Gonda <chardane@gmail.com>
2021-08-01 23:53:43 -0700

    React to messages in an MQTT topic

--- firmware/code.py
+++ firmware/code.py
@@ -12,6 +12,19 @@ from secrets import secrets
 
 from adafruit_esp32spi import adafruit_esp32spi_socket as socket
 from adafruit_minimqtt import adafruit_minimqtt as mqtt
+import neopixel
+
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+PURPLE = (180, 0, 255)
+ORANGE = (155, 50, 0)
+OFF = (0,0,0)
+PINK = (231,84,128)
+
+io_led = neopixel.NeoPixel(board.D7, 1, brightness=0.5, pixel_order=neopixel.RGB)
+io_led.fill(OFF)
 
 cs = DigitalInOut(board.ESP_CS)
 busy = DigitalInOut(board.ESP_BUSY)
@@ -70,16 +83,39 @@ def move_right():
     servo2.angle = 90
     time.sleep(servo_pause)
 
+def connected(client, userdata, flags, rc):
+    print("connected!")
+    robot_cat_topic = secrets["aio_username"] + "/feeds/robot-cat-topic"
+    client.subscribe(robot_cat_topic)
+    io_led.fill(CYAN)
+    
+def disconnected(client, userdata, rc):
+    print("disconnected :(")
+    io_led.fill(RED)
+    
+def message(client, topic, message):
+    print("message: %s", message)
+    io_led.fill(PINK)
+    
+    if "left" in message:
+        move_left()
+    elif "right" in message:
+        move_right()
+    else:
+        move_both()
+    
+    io_led.fill(CYAN)
+
 wifi.connect()
 
 mqtt.set_socket(socket, esp)
 mqtt_client = mqtt.MQTT(broker="io.adafruit.com",
-                        username=secrets["aio_username"],
-                        password=secrets["aio_key"])
+                        username = secrets["aio_username"],
+                        password = secrets["aio_key"])
 
 mqtt_client.on_connect = connected
 mqtt_client.on_disconnect = disconnected
-mqtt_client.on_message = message
+mqtt_client.on_message = message 
 
 mqtt_client.connect()
 
